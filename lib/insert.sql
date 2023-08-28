d8f7ee2a312c5f37a413bd44640c21f9ce08cd3b
Neon Ratemo <neon.ratemo@student.moringaschool.com>
2023-08-28 12:09:14 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 24, 'M', "chocolate", "high", "true" );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 27, 'M', "Black", "low", "true" );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 21, 'F', "White", "medium", "true" );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 31, 'M', "Brown", "high", "true" );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 29, 'F', "Black", "low", "true" );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 36, 'M', "Black", "high", "true" );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 33, 'F', "Black", "low", "true" );
